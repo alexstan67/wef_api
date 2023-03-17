7e5c70e55eecb005560056cd708ac7fb214a7e22
Alexandre Stanescot <alex@aerostan.com>
2023-03-17 18:32:59 +0100

    api basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   #get 'subscribers/index'
+  
+  namespace :api do
+    namespace :v1 do
+      get "/tests", to: "tests#index"
+    end
+  end
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     get 'pages/console'
