d7ac1c4ef36922f1d07d386a1da71e6d76790c93
Alexandre Stanescot <alex@aerostan.com>
2023-03-18 21:23:54 +0100

    Fix: wrong reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_many :blogs, :dependent => :delete_all
-
 # Remainings timeoutable :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
