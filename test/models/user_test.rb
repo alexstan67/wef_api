815236e14b99d6c5abe235b362192c1291e920e1
Alexandre Stanescot <alex@aerostan.com>
2023-03-20 19:40:50 +0100

    Test Model user setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Correct user should save" do
+    user = users(:john)
+    assert user.save
+  end
+
+  test "email validation should trigger" do
+    assert_not User.new(password: "123456", first_name: "First name", last_name: "Last Name").save
+  end
+
+  test "Too short password should not save" do
+    assert_not User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123").save
+  end
 end
