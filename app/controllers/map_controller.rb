86c37dc634ca53cabf6beb8d1e368037b99a3b6b
Alexandre Stanescot <alex@aerostan.com>
2023-05-22 19:50:02 +0200

    leaflet basic forms setup

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,28 @@
+require 'rgeo'
+require 'rgeo/geo_json'
+
 class MapController < ApplicationController
   def index
+    # We load a specific weather polygon
+    # We convert rgeo polygon format into GeoJSON
+    weatherTile = WeatherTile.last
+    @weatherGeoJson = RGeo::GeoJSON.encode(weatherTile.geom).to_json
+
+    # We load 1 airport marker
+    airport = Airport.find_by(icao: "ELLX").lonlat
+    @airportGeoJson = RGeo::GeoJSON.encode(airport).to_json
+
+    # We load all airports markers
+    @airports_array = []
+    airports = Airport.all
+    airports.each do |airport|
+      @airports_array.push({ :name => airport.name,
+                            :icao => airport.icao,
+                            :geojson => RGeo::GeoJSON.encode(airport.lonlat)
+      })
+    end 
+
+    #@airportsGeoJson = RGeo::GeoJSON.encode(airports).to_json
+
   end
 end
--- app/javascript/controllers/osm_controller.js
+++ app/javascript/controllers/osm_controller.js
@@ -1,13 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  /*
   static values = {
-    markers: Array,
-    center: Array,
-    radius: Number
+    tile: Object,
+    airport: Object,
+    airports: Array
   }
-  */
 
   static targets = [ 'map' ]
 
@@ -17,71 +15,38 @@ export default class extends Controller {
   }
 
   displayMap() {
-    // Variables
-    //let markersTable = [];
- 
-    // We create a new map
-    //this.map = L.map(this.mapTarget).setView([this.centerValue[0].lat, this.centerValue[0].lon], 6);
+    // We display the map
     this.map = L.map(this.mapTarget).setView([49.6 ,6.2], 6);
-    
-    // We display the tile layer
-    // Standard map
-    /*
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(this.map);
-    */
+
+    // We define the tile layer
     L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
       maxZoom: 19,}).addTo(this.map);
 
-    // Topographic Map
-    /*
-    L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', { maxZoom: 17, attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}).addTo(this.map);
-    */
+    // We add the weather tile
+    const geojsonLayer = L.geoJSON(this.tileValue).addTo(this.map);
+    this.map.fitBounds(geojsonLayer.getBounds());
 
-    // We display the homebase marker
-    //var marker = L.marker([this.centerValue[0].lat, this.centerValue[0].lon]).addTo(this.map);
+    // We display the airport homebase marker
+    var myStyle = {
+      "color": "#ff7800",
+      "weight": 5,
+      "opacity": 0.65
+    };
+    L.geoJSON(this.airportValue, { style: myStyle }).addTo(this.map)
 
-    // We display a circle radius representing the chosen distance
-    /*
-    var circle = L.circle([this.centerValue[0].lat, this.centerValue[0].lon], {
-      color: '#FF8600',
-      fillColor: '#FF8600',
-      fillOpacity: 0.2,
-      radius: this.radiusValue
-    }).addTo(this.map);
-
-    // We display the destination airports markers and assign a popup
-    this.markersValue.forEach((airport) => {
-      // We create a marker for each airport
-      //var marker = L.marker([airport.lat, airport.lon]).addTo(this.map);
-      var iconDestination = L.icon({
-      iconUrl: airport.image_url,
-      iconSize:     [50, 50], // size of the icon
-      iconAnchor:   [25, 49], // point of the icon which will correspond to marker's location
-      popupAnchor:  [0, -50] // point from which the popup should open relative to the iconAnchor
-      });
-
-      marker = L.marker([airport.lat, airport.lon], {icon: iconDestination}).addTo(this.map);
-
-      // We add the marker to a table in order used to fit the view on the markers
-      markersTable.push(marker);
-
-      // We add a popup to each marker
-      marker.bindPopup(airport.info_window).openPopup();
+    // We display all airports
+    this.airportsValue.forEach((airport) => {
+      var circle = L.circle([airport.geojson.coordinates[1], airport.geojson.coordinates[0]], {
+      //var circle = L.circle([48.0, 6.0], {
+        color: '#FF8600',
+        fillColor: '#FF8600',
+        fillOpacity: 0.2,
+        radius: 1000
+      }).addTo(this.map);
     });
 
-    // We create a group representating the featureBounds object
-    var group = new L.featureGroup(markersTable);
-
-    // We adapt the zoom on the group
-    this.map.fitBounds(group.getBounds().pad(0.5));
-  */
   }
   disconnect(){
     this.map.remove()
   }
 }
-
-
